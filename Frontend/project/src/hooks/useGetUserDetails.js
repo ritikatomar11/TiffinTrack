cfd93f4c41438f2f59bfc819c0623c0ec1adedb7
Ritika tomar <ritika.tomar.0984@gmail.com>
2025-09-21 10:50:08 +0530

    getUserDetails

--- Frontend/project/src/hooks/useGetUserDetails.js
+++ Frontend/project/src/hooks/useGetUserDetails.js
@@ -1,17 +1,33 @@
-import React , {useEffect} from "react"; 
+// import React , {useEffect} from "react"; 
+// import { selectLoggedInUser } from "../features/auth/AuthSlice";
+// import { useDispatch , useSelector } from "react-redux";
+// import { getUserDetailsAsync } from "../features/user/UserSlice";
+
+// export const useGetUserDetails = (deps)=>{
+//     const loggedInUser = useSelector(selectLoggedInUser)
+//     const dispatch = useDispatch(); 
+//     console.log("inside" , loggedInUser)
+
+//     useEffect(()=>{
+//         if(deps && loggedInUser){
+//             console.log("id" , loggedInUser.data._id)
+//             dispatch(getUserDetailsAsync(loggedInUser.data._id))
+//         }
+//     } , [deps , loggedInUser])
+// }
+
+import { useEffect } from "react"; 
+import { useDispatch, useSelector } from "react-redux";
 import { selectLoggedInUser } from "../features/auth/AuthSlice";
-import { useDispatch , useSelector } from "react-redux";
 import { getUserDetailsAsync } from "../features/user/UserSlice";
 
-export const useGetUserDetails = (deps)=>{
-    const loggedInUser = useSelector(selectLoggedInUser)
-    const dispatch = useDispatch(); 
-    console.log("inside" , loggedInUser)
+export const useGetUserDetails = () => {
+  const dispatch = useDispatch();
+  const loggedInUser = useSelector(selectLoggedInUser);
 
-    useEffect(()=>{
-        if(deps && loggedInUser){
-            console.log("id" , loggedInUser.data._id)
-            dispatch(getUserDetailsAsync(loggedInUser.data._id))
-        }
-    } , [deps , loggedInUser])
-}
+  useEffect(() => {
+    if (loggedInUser?.data?._id) {
+      dispatch(getUserDetailsAsync(loggedInUser.data._id));
+    }
+  }, [dispatch, loggedInUser?.data?._id]); // only re-run when the user ID changes
+};
